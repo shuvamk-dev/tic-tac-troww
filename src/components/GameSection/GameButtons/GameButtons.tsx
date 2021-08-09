98266791996a34278aaca920056257a24aad7474
Shuvam Kumar <shuvamkumar@Shuvams-MacBook-Pro.local>
2021-08-10 00:35:36 +0530

    Add action buttons

--- src/components/GameSection/GameButtons/GameButtons.tsx
+++ src/components/GameSection/GameButtons/GameButtons.tsx
@@ -1,10 +1,27 @@
+import './gameButtons.css';
+
 import React from 'react';
 
 const GameButtons = () => {
+  const handleUndoBtnClick = () => {
+    console.log("Undo Done");
+  };
+  const handleMuteBtnClick = () => {
+    console.log("Mute Done");
+  };
+  const handleResetBtnClick = () => {
+    console.log("Game Reset");
+  };
   return (
-    <div>
-      <div>
-        <i className="fi-rr-undo"></i>
+    <div className="gbs01Container">
+      <div className="gbs01Icon" onClick={handleUndoBtnClick}>
+        <i className="fi-rr-undo flaticon"></i>
+      </div>
+      <div className={`gbs01Icon gbs10MuteIcon`} onClick={handleMuteBtnClick}>
+        <i className="fi-rr-volume flaticon"></i>
+      </div>
+      <div className="gbs01Icon" onClick={handleResetBtnClick}>
+        <i className="fi-rr-trash flaticon"></i>
       </div>
     </div>
   );
